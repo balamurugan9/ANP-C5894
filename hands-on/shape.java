9035a18fefeb213c57b6cb80cb71cdc12de2bb52
balamurugan9 <137864001+balamurugan9@users.noreply.github.com>
2023-07-05 14:04:30 +0530

    Add files via upload

---
+++ hands-on/shape.java
@@ -0,0 +1,27 @@
+package oops;
+
+class shape {
+	
+	    public void calculateArea() {
+	        System.out.println("Calculating area of the shape.");
+	    }
+	}
+
+	class Rectangle extends shape {
+		public void calculateArea() {
+		System.out.println("Calculating area of the Rectangle.");
+		
+		}
+		
+		class main {
+			
+      public static void main(String[] args) {
+    	  
+    	 Rectangle r=new Rectangle();
+    	 r.calculateArea();
+      }
+		
+
+	}
+
+}
